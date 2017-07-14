7e948d8df0159bbd5507a1f33e57ec0ae571a4f5
马筱静 <maxiaojing@maxiaojingdeMacBook-Pro.local>
2017-07-14 10:27:57 +0800

    add add_to_cart button

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
    devise_for :users
    namespace :admin do
    resources :products
- end
-   resources :products
+   end
+   
+   resources :products do
+    member do
+      post :add_to_cart
+   end
+  end
 end
