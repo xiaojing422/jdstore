e69ed9876b64822db8eb5c0ae98e06fd033673be
马筱静 <maxiaojing@maxiaojingdeMacBook-Pro.local>
2017-07-14 11:42:35 +0800

    empty cart

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,11 @@ Rails.application.routes.draw do
     member do
       post :add_to_cart
    end
+ end
+
+   resources :carts do
+   collection do
+   delete :clean
+   end
   end
-    resources :carts
-end
+ end
